7dd032ca6eda612f3be00ee8e450531e649bef19
Nicolas Mohr <81moni1bif@c-143-002.ad.hft-stuttgart.de>
2018-05-03 21:00:14 +0200

    prototype store login implementation

--- sqlscript.sh
+++ sqlscript.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
-PW="$(cat ~/Dokumente/sqlpw)"
-mysql -h 193.196.143.168 -u dk8s_81moni1bif -p"$PW"
+delimiter=";"
+if [ ! -f  ~/.sqllogin]; then
+  echo "No login file found in home, creating a new one."
+  echo "WARNING:  use only in secure environments, data is stored in plain text"
+  echo "Please enter your username"
+  read username
+  echo "Hi $username, nice to meet you."
+  echo "Please enter your password."
+  read pw
+  echo "${username}${delimiter}${password}" >  ~/.sqllogindata
+fi
+
+login="$(cat ~/.sqlllogindata)"
+IFS=\\$delimiter read -a loginarr  <<<"$login"
+mysql -h 193.196.143.168 -u "$loginarr[0]" -p"$loginarr[1]"
