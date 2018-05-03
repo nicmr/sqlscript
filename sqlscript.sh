072dad3b05c134a87d3772708d1393feb0f7998e
Nicolas Mohr <81moni1bif@c-143-002.ad.hft-stuttgart.de>
2018-05-03 21:23:17 +0200

    major update, saving and using login features working flawlessly

--- sqlscript.sh
+++ sqlscript.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 delimiter=";"
-if [ ! -f  ~/.sqllogin]; then
-  echo "No login file found in home, creating a new one."
-  echo "WARNING:  use only in secure environments, data is stored in plain text"
-  echo "Please enter your username"
+if [ ! -f  ~/.sqllogindata ]; then
+  echo ">No login file found in home, creating a new one."
+  echo ">WARNING:  use only in secure environments, data is stored in plain text"
+  echo ">Please enter your username"
   read username
-  echo "Hi $username, nice to meet you."
-  echo "Please enter your password."
-  read pw
+  echo ">Hi $username, nice to meet you."
+  echo ">Please enter your password."
+  read password
   echo "${username}${delimiter}${password}" >  ~/.sqllogindata
 fi
 
-login="$(cat ~/.sqlllogindata)"
+login="$(cat ~/.sqllogindata)"
 IFS=\\$delimiter read -a loginarr  <<<"$login"
-mysql -h 193.196.143.168 -u "$loginarr[0]" -p"$loginarr[1]"
+echo "${loginarr[0]}"
+echo "${loginarr[1]}"
+mysql -h 193.196.143.168 -u "${loginarr[0]}" -p"${loginarr[1]}"
